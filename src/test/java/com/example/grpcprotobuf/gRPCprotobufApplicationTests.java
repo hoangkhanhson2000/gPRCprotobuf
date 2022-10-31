6e43565fa112a69ae5868f28aeebe360ce4114b5
hoangkhanhson2000 <hoangkhanhson2000@gmail.com>
2022-10-31 10:44:56 +0700

    Initial commit

---
+++ src/test/java/com/example/grpcprotobuf/gRPCprotobufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcprotobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class gRPCprotobufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
